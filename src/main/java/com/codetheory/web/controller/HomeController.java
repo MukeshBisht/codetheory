126081e8585a469f8e81c9f631e4631f938bb91f
Mukesh Bisht <mukesh.bisht54@gmail.com>
2018-01-14 14:38:18 +0530

    first commit

---
+++ src/main/java/com/codetheory/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.codetheory.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String Home(Model model) {
+		return "home";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,11 @@
+<%@page contentType = "text/html;charset = UTF-8" language = "java" %>
+<%@page isELIgnored = "false" %>
+<%@taglib uri = "http://www.springframework.org/tags/form" prefix = "form"%>
+<html>
+   <head>
+      <title>Home</title>
+   </head>
+   <body> 
+       <h1>welcome</h1>
+   </body>
+</html>
