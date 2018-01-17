3e1349aeb0ef50961ca20c6503ed22f7b139051a
zubercuber <30353375+zubercuber@users.noreply.github.com>
2018-01-17 09:11:33 +0530

    quiz

---
+++ src/main/java/com/codetheory/web/controller/QuizController.java
@@ -0,0 +1,27 @@
+package com.codetheory.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.codetheory.web.model.QuizQuestion;
+
+@Controller
+public class QuizController {
+
+	@RequestMapping(value = "/quiz", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public QuizQuestion createQuiz() {
+		QuizQuestion question = new QuizQuestion();
+		question.setQuestion("What is JAVA?");
+		question.setCorrect("JAVA IS AN OOP LANGUAGE");
+			String options[] = new String[4];
+			options[0] = "JAVA IS AN OOP LANGUAGE";
+			options[1] = "JAVA IS AN OOD LANGUAGE";
+			options[2] = "JAVA IS AN OOA LANGUAGE";
+			options[3] = "JAVA IS AN OBP LANGUAGE";
+		question.setOptions(options);AGE");
+		return question;
+	}
+}
---
+++ src/main/java/com/codetheory/web/model/QuizQuestion.java
@@ -0,0 +1,32 @@
+package com.codetheory.web.model;
+
+public class QuizQuestion {
+    private String question;
+    private String options[];
+    private String correct;
+
+    public QuizQuestion() {
+
+    }
+
+    public void setQuestion (String question){
+        this.question = question;
+    }
+    public String getQuestion (){
+        return question;
+    }
+    public void setOptions (String options[]){
+        for (int i=0; i<options.length; i++)
+            this.options[i] = options[i];
+    }
+    public String[] getOptions (){
+        return options;
+    }
+
+    public void setCorrect (String correct) {
+        this.correct = correct;
+    }
+    public String getCorrect (){
+        return correct;
+    }
+}
